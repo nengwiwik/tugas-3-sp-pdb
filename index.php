6d6602decff3b4704032fffdbc1570d2406c1188
nengwiwik <wiwiklistiyarini545@gmail.com>
2022-01-27 07:29:42 +0700

    init

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wiwik Listiyarini 41119051</title>
+</head>
+
+<body>
+    <h2>Form Input Barang</h2>
+    <form action="" method="post">
+        <label for="kode">Kode Barang</label>
+        <br>
+        <input type="text" name="kode" id="kode">
+
+        <br><br>
+
+        <label for="nama">Nama Barang</label>
+        <br>
+        <input type="text" name="nama" id="nama">
+
+        <br><br>
+
+        <label for="harga">Harga</label>
+        <br>
+        <input type="text" name="harga" id="harga">
+
+        <br><br>
+
+        <input type="submit" value="Submit">
+    </form>
+
+    <p>Tombol "submit" belum dapat berfungsi karena belum ada file database.</p>
+</body>
+
+</html>
